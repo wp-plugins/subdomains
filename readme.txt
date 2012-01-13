462e9384971a2e3f5737585bdc34f1a3fb152e47
pankajanupam <pankajanupam@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-13 08:35:48 +0000

    version 0.5 update
    git-svn-id: https://plugins.svn.wordpress.org/subdomains/branches/0.3@489285 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,60 @@
+=== Subdomains ===
+Contributors: pankajanupam
+Tags: subdomains, categories, subdomain
+Requires at least: 2.8
+Tested up to: 3.3.1
+
+Subdomains allows users to setup there main categories as subdomains. It's a lite and fast code.
+
+== Description ==
+
+Subdomains allows users to setup there main categories as subdomains. It's a lite and fast code that setup all main categories as subdomains.
+
+Please leave a commment at http://pankajanupam.in/wordpress-plugins/subdomains/ for any issue & help me to improve the plugin.
+I will reply within 4 hours.
+
+Note: Please set permalinks to /%category%/%postname%/
+
+== Installation ==
+
+You can install Subdomains directly from the WordPress admin panel.
+
+	Visit the Plugins > Add New and search for 'Subdomains'.
+
+	Click to install. 
+	
+	Once installed, activate and it is functional.
+
+
+Manual Installation Subdomains plugin:
+
+    Download and unzip the file
+
+    Upload the full extracted folder to the /wp-content/plugins/ directory
+
+    Activate the plugin through the 'Plugins' menu in WordPress.
+
+You're done! The Plugin ready to use according to the directions in the description.
+
+
+== Frequently Asked Questions ==
+
+= Where can I see this in action =
+
+My blog is based on this plugin: http://pankajanupam.com
+
+== Screenshots ==
+
+None
+
+== Changelog ==
+
+= 0.3 =
+remove double backslash
+add child categories support
+
+= 0.2 =
+add tags
+
+= 0.1 =
+Beta Release
---
+++ subdomains.php
@@ -0,0 +1,131 @@
+<?php
+/*
+Plugin Name: Subdomains
+Plugin URI: http://pankajanupam.in/wordpress-plugins/subdomains/
+Description: Use selecttive categories as subdomain
+Version: 0.4
+Author: PANKAJ ANUPAM
+Author URI: http://pankajanupam.in
+
+* LICENSE
+    Copyright 2011 PANKAJ ANUPAM  (email : mymail.anupam@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
+<?php
+class subSubdomain{
+    var $slug;
+    var $field;
+    function  __construct() {
+        $this->field ='category_name';
+    }
+
+    function getSubdomain(){
+        $url = getenv( 'HTTP_HOST' );
+	$domain = explode( ".", $url );
+        $this->slug = $domain[0];
+        return get_category_by_slug($this->slug);
+    }
+
+    function getRewriteRules(){
+        $rules = array();
+	$rules["feed/(feed|rdf|rss|rss2|atom)/?$"] = "index.php?" . $this->field . "=" . $this->slug . "&feed=\$matches[1]";
+	$rules["(feed|rdf|rss|rss2|atom)/?$"] = "index.php?" . $this->field . "=" . $this->slug . "&feed=\$matches[1]";
+	$rules["page/?([0-9]{1,})/?$"] = "index.php?" . $this->field . "=" . $this->slug . "&paged=\$matches[1]";
+	$rules["$"] = "index.php?" . $this->field . "=" . $this->slug;
+        return $rules;
+    }
+}
+
+class initPlugin extends subSubdomain{
+    function __construct(){
+        parent::__construct();
+    }
+    function addActions() {
+		add_action( 'init', 'wps_init', 2 );
+	}
+    function addFilters(){
+     //   add_filter( 'rewrite_rules_array', 'wps_rewrite_rules' );
+        add_filter( 'category_rewrite_rules', 'sub_category_rewrite_rules' );
+        add_filter( 'post_rewrite_rules', 'sub_post_rewrite_rules' );
+
+        add_filter( 'category_link', 'sub_category_link', 10, 2 );
+        add_filter( 'post_link', 'sub_post_link', 10, 2 );
+    }
+}
+
+$obj_sub = new initPlugin;
+$obj_sub->addActions();
+$obj_sub->addFilters();
+
+function wps_init () {
+	if (!is_admin()) {
+		// Stuff changed in WP 2.8
+		if (function_exists('set_transient')) {
+			set_transient('rewrite_rules', "");
+			update_option('rewrite_rules', "");
+		} else {
+			update_option('rewrite_rules', "");
+		}
+	}
+}
+
+add_filter( 'root_rewrite_rules', 'wps_root_rewrite_rules' );
+function wps_root_rewrite_rules( $rules ) {
+        //$rules = array();
+return $rules;
+}
+function sub_category_rewrite_rules($rules){
+    global $obj_sub;
+  if($domain = $obj_sub->getSubdomain()){
+        $rules = $obj_sub->getRewriteRules();
+  }
+  return $rules;
+}
+
+function sub_post_rewrite_rules($rules){
+$rules = array ();
+    $rules['[^/]+/attachment/([^/]+)/?$']='index.php?attachment=$matches[1]';
+    $rules['[^/]+/attachment/([^/]+)/trackback/?$'] ='index.php?attachment=$matches[1]&tb=1';
+    $rules['[^/]+/attachment/([^/]+)/feed/(feed|rdf|rss|rss2|atom)/?$'] ='index.php?attachment=$matches[1]&feed=$matches[2]';
+    $rules['[^/]+/attachment/([^/]+)/(feed|rdf|rss|rss2|atom)/?$'] ='index.php?attachment=$matches[1]&feed=$matches[2]';
+    $rules['[^/]+/attachment/([^/]+)/comment-page-([0-9]{1,})/?$'] ='index.php?attachment=$matches[1]&cpage=$matches[2]';
+    $rules['([^/]+)/trackback/?$'] ='index.php?name=$matches[1]&tb=1';
+    $rules['([^/]+)/feed/(feed|rdf|rss|rss2|atom)/?$'] = 'index.php?name=$matches[1]&feed=$matches[2]';
+    $rules['([^/]+)/(feed|rdf|rss|rss2|atom)/?$'] ='index.php?name=$matches[1]&feed=$matches[2]';
+    $rules['([^/]+)/page/?([0-9]{1,})/?$'] ='index.php?name=$matches[1]&paged=$matches[2]';
+    $rules['([^/]+)/comment-page-([0-9]{1,})/?$'] ='index.php?name=$matches[1]&cpage=$matches[2]';
+    $rules['([^/]+)(/[0-9]+)?/?$']='index.php?name=$matches[1]&page=$matches[2]';
+    $rules['[^/]+/([^/]+)/?$'] ='index.php?attachment=$matches[1]';
+    $rules['[^/]+/([^/]+)/trackback/?$'] ='index.php?attachment=$matches[1]&tb=1';
+    $rules['[^/]+/([^/]+)/feed/(feed|rdf|rss|rss2|atom)/?$'] ='index.php?attachment=$matches[1]&feed=$matches[2]';
+    $rules['[^/]+/([^/]+)/(feed|rdf|rss|rss2|atom)/?$'] = 'index.php?attachment=$matches[1]&feed=$matches[2]';
+    $rules['[^/]+/([^/]+)/comment-page-([0-9]{1,})/?$']= 'index.php?attachment=$matches[1]&cpage=$matches[2]';
+    return $rules;
+}
+
+function sub_category_link( $link, $term_id ) {
+    $link = preg_replace('/(?<=http\:\/\/)([a-z0-9_\-\.]+)\/category(.*)\/([a-z0-9_\-]+)/','$3.$1', $link);
+    return $link;
+}
+
+function sub_post_link( $link, $id ){
+   $link = preg_replace('/(?<=http\:\/\/)([a-z0-9_\-\.]+)\/(.*)\/([a-z0-9\-\_]+)\/([a-z0-9\-\_]+)/','$3.$1/$4', $link);
+   $link = preg_replace('/(?<=http\:\/\/)([a-z0-9_\-\.]+)\/([a-z0-9\-\_]+)\/([a-z0-9\-\_]+)/','$2.$1/$3', $link);
+   return $link;
+}
+
+?>
